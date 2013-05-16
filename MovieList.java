be1108e7eb7e2bee248906f8cd633cbcd9c32984
Corban Mailloux <github@corb.co>
2013-05-16 00:26:15 -0300

    MovieList.java, first complete version

---
+++ MovieList.java
@@ -0,0 +1,130 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Observable;
+
+/**
+ * MovieList.java
+ *
+ * File:
+ *  $Id$
+ *
+ * Revisions:
+ *  $Log$
+ * 
+ */
+
+/**
+ * A new version of MovieList, using Java 1.7.
+ * 
+ * @author Corban Mailloux <cdm3806@rit.edu>
+ * 
+ */
+public class MovieList extends Observable {
+
+  /**
+   * The list of folders to search through
+   */
+  private static List<File> folderList = new ArrayList<File>();
+
+  /**
+   * The complete MovieList
+   */
+  private static List<Movie> HdMovieList = new ArrayList<Movie>();
+
+  /**
+   * The MovieList that represents the current state
+   */
+  private static List<Movie> currMovieList = new ArrayList<Movie>();
+
+  public MovieList() {
+    folderList.add(new File("J:\\Media\\Movies"));
+    folderList.add(new File("P:\\Media\\Movies"));
+    folderList.add(new File("Q:\\Media\\Movies"));
+    folderList.add(new File("S:\\Media\\Movies"));
+
+    // For each folder
+    for (File folder : folderList) {
+      // If the folder doesn't exist, skip it
+      if (!folder.exists() || !folder.isDirectory()) {
+        continue;
+      }
+
+      // For each movie in this folder
+      for (File movie : folder.listFiles()) {
+        if (movie.isFile()) {
+          if (movie.getName().equalsIgnoreCase("Thumbs.db")) {
+            continue;
+          }
+          // Add it to the HdMovieList
+          HdMovieList.add(new Movie(movie));
+        }
+      }
+    }
+
+    // Sort the movie list
+    Collections.sort(HdMovieList);
+    // System.out.println(HdMovieList);
+
+    // for (Movie m1 : HdMovieList) {
+    // System.out.println(m1.getName());
+    // }
+    System.out.println("Total Movies: " + HdMovieList.size());
+
+    // Set up the current MovieList
+    currMovieList.addAll(HdMovieList);
+  }
+
+  /**
+   * A forced call to update.
+   */
+  public void update() {
+    setChanged();
+    notifyObservers();
+  }
+
+  /**
+   * Open a given Movie in Windows Explorer.
+   * 
+   * @param m1
+   *          - the Movie to open and select
+   */
+  public void openExplorer(Movie m1) {
+    try {
+      Runtime.getRuntime().exec("explorer /select, " + m1.getPath());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Return the current MovieList. Called by Observer's update.
+   * 
+   * @return - current MovieList
+   */
+  public List<Movie> getMovieList() {
+    return currMovieList;
+  }
+
+  /**
+   * Non-case-sensitive search of the current movie list for a string
+   * 
+   * @param searchStr
+   *          - the string to search for
+   */
+  public void search(String searchStr) {
+    currMovieList.clear();
+    for (Movie m1 : HdMovieList) {
+      if (m1.getName().toLowerCase().contains(searchStr.toLowerCase())) {
+        currMovieList.add(m1);
+      }
+    }
+    setChanged();
+    notifyObservers();
+    // return (currMovieList.size() > 0);
+
+  }
+
+}
